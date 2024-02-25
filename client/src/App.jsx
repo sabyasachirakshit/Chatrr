1428adaf3e21e1c4e3f98d2356261700b06732c1
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2024-02-25 14:05:34 +0530

    router imported

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
