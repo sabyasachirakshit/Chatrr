eebc0788e6ab3a374aaff57ee7f591a53ee5a234
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2024-02-25 19:47:02 +0530

    register added

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { Button, Container, Paper, TextField, Typography } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Container,
+  IconButton,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
-
+import { CameraAlt as CameraAltIcon } from "@mui/icons-material";
 function Login() {
   const [isLogin, setIsLogin] = useState(true);
 
-  const toggleLogin = () => setIsLogin(false);
+  const toggleLogin = () => setIsLogin((prev) => !prev);
   return (
     <Container
       component={"main"}
@@ -68,7 +77,72 @@ function Login() {
             </form>
           </>
         ) : (
-          <span>Register</span>
+          <>
+            <Typography variant="h5">Sign Up</Typography>
+            <form style={{ width: "100%", marginTop: "1rem" }}>
+              <Stack position={"relative"} width={"1rem"} margin={"auto"}>
+                <Avatar
+                  sx={{ width: "10rem", height: "10rem", objectFit: "contain" }}
+                />
+
+                <IconButton>
+                  <>
+                    <CameraAltIcon />
+                  </>
+                </IconButton>
+              </Stack>
+
+              <TextField
+                required
+                fullWidth
+                label="Name"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="Bio"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="Username"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                margin="normal"
+                variant="outlined"
+              />
+              <Button
+                sx={{ marginTop: "1rem" }}
+                variant="contained"
+                color="primary"
+                type="submit"
+                fullWidth
+              >
+                Sign Up
+              </Button>
+              <Typography textAlign={"center"} margin={"1rem"}>
+                OR
+              </Typography>
+              <Button
+                variant="text"
+                color="secondary"
+                fullWidth
+                onClick={toggleLogin}
+              >
+                log in instead
+              </Button>
+            </form>
+          </>
         )}
       </Paper>
     </Container>
